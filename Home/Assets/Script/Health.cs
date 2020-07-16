85ff33c18a7256c2af2fb00d5856eb42de85c1e1
Jian Chen <jc@advancedj.com>
2020-07-15 21:45:16 -0700

    added new project

---
+++ Home/Assets/Script/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int curHealth = 0;
+    public int maxHealth = 10000;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DamagePlayer(10);
+    }
+
+    public void DamagePlayer( int damage )
+    {
+        curHealth -= damage;
+
+        healthBar.SetHealth( curHealth );
+    }
+}
